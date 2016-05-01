462dac1603b9d138dddae247d3bf2c21e6ec0c56
Bruno Al. Brito <brunoalbuquerquebrito@outlook.com>
2016-05-01 13:26:11 -0300

    Criação do Menu
    Menu para selecão das opçõe devolvendo numero correspondente a escolha

---
+++ LP1_Gestao_academica/GestaoAcademica/src/versao1/GestaoAcademicaApp1.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package versao1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Bruno
+ */
+public class GestaoAcademicaApp1 {
+
+    public static void main(String[] args) {
+        //Usuario irá informar sua escolha para o menu
+        selection(choose());
+    }
+
+    //Menu com as opções 
+    public static void selection(int choose) {
+        switch (choose) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    //Dados seleciodos pelo menu;
+    public static int choose() {
+        int resp = 0;
+        try {
+            resp = Integer.parseInt(JOptionPane.showInputDialog("Informe sua escolha:\n"
+                    + "1 - Listar número e nome de todos estudande \n"
+                    + "2 - Listar o código de todas as diciplicas \n"
+                    + "3 - Listar todas as informações dos estudande matricula-dos em uma determina diciplica \n"
+                    + "4 - Listar todas as informações de uma diciplica em que um determinado estudante está matriculado"));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Valores não informados corretamente");
+        }
+
+        return resp;
+    }
+}
