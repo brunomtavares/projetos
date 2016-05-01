a71f9d164401a64a6dd01ab374d2898ad19356b3
Bruno Al. Brito <brunoalbuquerquebrito@outlook.com>
2016-05-01 16:29:42 -0300

    Versão Final Commit Master 1

--- LP1_Gestao_academica/GestaoAcademica/src/versao1/GestaoAcademicaApp1.java
+++ LP1_Gestao_academica/GestaoAcademica/src/versao1/GestaoAcademicaApp1.java
@@ -12,12 +12,14 @@ import javax.swing.JOptionPane;
  * @author Bruno
  */
 public class GestaoAcademicaApp1 {
-    
+
     public static Universidade mackenzie;
+    public static String constAluno = "";
+    
 
     public static void main(String[] args) {
         mackenzie = new Universidade("Mackenzie");
-        mackenzie.carregarDadosArquivo("disciplinas-abddcdccebbdcebdbadd", "estudantes-abddcdccebbdcebdbadd", "matriculas-abddcdccebbdcebdbadd");
+        mackenzie.carregarDadosArquivo("disciplinas-abddcdccebbdcebdbadd.txt", "estudantes-abddcdccebbdcebdbadd.txt", "matriculas-abddcdccebbdcebdbadd.txt");
         //Usuario irá informar sua escolha para o menu
         selection(choose());
     }
@@ -26,12 +28,14 @@ public class GestaoAcademicaApp1 {
     public static void selection(int choose) {
         switch (choose) {
             case 1:
+                showAlunos();
                 break;
             case 2:
                 break;
             case 3:
                 break;
             case 4:
+                allDiciplinasEstudante();
                 break;
             default:
                 break;
@@ -49,13 +53,52 @@ public class GestaoAcademicaApp1 {
                     + "3 - Listar todas as informações dos estudande matricula-dos em uma determina diciplica \n"
                     + "4 - Listar todas as informações de uma diciplica em que um determinado estudante está matriculado"));
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"Valores não informados corretamente");
+            JOptionPane.showMessageDialog(null, "Valores não informados corretamente");
         }
 
         return resp;
     }
-    
-    public static void getDisciplinas2(){
-        //teste test tes
+
+    public static void showAlunos() {
+        for (Estudante estudante : mackenzie.getEstudantes()) {
+            constAluno += ("Número: " + estudante.getId() + " Nome: " + estudante.getNome() + "\n");
+        }
+        JOptionPane.showMessageDialog(null, constAluno);
+    }
+
+    public static void allDiciplinasEstudante() {
+        String constInfMatridoAluno = "";
+        int credito = 0;
+        //Exibe os alunos matriculados
+        for (Estudante estudante : mackenzie.getEstudantes()) {
+            constAluno += ("Número: " + estudante.getId() + " Nome: " + estudante.getNome() + "\n");
+        }
+        
+        
+        long matricula = infMatricula();
+        
+        
+        //Percorre todos os estudantes
+        for (Estudante estudantes : mackenzie.getEstudantes()) {
+            //Seleciona apenas o que tiver a matricula informada
+            if (estudantes.getId() == matricula) {
+                //Como sabemos a posição em que ele se encontra iremos pegar o tamanho 
+                //do array de diciplinas deste aluno e percorre-la
+                for (Disciplina disciplinas : estudantes.getDisciplinasMatriculadas()) {
+                    //A partir do array de diciplinas iremos pegar seu códio
+                    constInfMatridoAluno += (disciplinas.getCodigo() + "\n");
+                    //Somar os créditos de cada disciplina
+                    credito += disciplinas.getCreditos();
+                }
+            }
+        }
+
+        exibeInfDisplina(constInfMatridoAluno, credito);
+    }
+    public static long infMatricula(){
+        return Long.parseLong(JOptionPane.showInputDialog(null, constAluno + " Informe sua matrícula"));
+    }
+    public static void exibeInfDisplina(String constInfMatridoAluno,int credito){
+        JOptionPane.showMessageDialog(null, constInfMatridoAluno + "\n Total de Crédito: " + credito);
     }
 }
